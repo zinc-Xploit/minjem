642bda19f15ed22627fc1948caeecf89df49ef0b
zinc-Xploit <76673400+zinc-Xploit@users.noreply.github.com>
2020-12-28 18:04:36 +0700

    Create datatable.js

---
+++ assets/js/examples/datatable.js
@@ -0,0 +1,13 @@
+'use strict';
+$(document).ready(function () {
+
+    $('#tools').DataTable({
+        responsive: true
+    });
+
+    $('#example2').DataTable({
+        "scrollY": "400px",
+        "scrollCollapse": true
+    });
+
+});
